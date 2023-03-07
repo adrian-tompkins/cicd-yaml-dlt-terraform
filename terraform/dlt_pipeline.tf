e2cc2d63b28fff4354fec5d3a9b52eec00284953
Adrian Tompkins <adrian.tompkins@databricks.com>
2023-03-07 22:53:05 +1000

    wip

--- terraform/dlt_pipeline.tf
+++ terraform/dlt_pipeline.tf
@@ -18,7 +18,7 @@ resource databricks_pipeline this {
   for_each = fileset("../pipelines/", "*.yaml")
   name    = trimsuffix(each.value, ".yaml")
   configuration = {
-    "pipeline.yaml_config_path" = "${databricks_repo.this.path}/notebooks/dlt_notebook/${each.value}"
+    "pipeline.yaml_config_path" = "${databricks_repo.this.path}/pipelines/${each.value}"
   }
   development = true
 
